c008d6defe3fee7eb46c1ce00239bc3d8bc62f17
predator27 <anand.naman997@gmail.com>
2025-02-04 12:22:13 +0530

    .

---
+++ week-5/week-5.2/map-filter-arrow.js/index.js
@@ -0,0 +1,38 @@
+// map and filter 
+
+// given an array, give me back a new array in which every value gets multiplied by 2 
+
+
+
+// method 01 //
+// const input2 = [1,2,3,4,5,6]
+
+// const newArr = []
+// for (let i = 0; i < input2.length; i ++){
+//     newArr.push(input2[i]*2)
+// }
+
+// console.log(newArr);
+
+
+// ************* map **************
+
+// whenever we are told to transform arrays, we use map 
+
+
+// inputArr.map(function we want to run on each elem on input arr)
+
+// const input = [1,2,3,4,5]
+// const ans = input.map((i)=>{
+//     return i * 2
+// })
+// console.log(ans);
+
+
+
+// ************* filter *********************
+
+// problem- given an input array, give back all the even values from it. 
+
+const arr = [1,2,3,4,5,6,7,8,9]
+
