0c69e03417e422d9edb0438fe410222b2a7ab5c7
predator27 <anand.naman997@gmail.com>
2025-02-23 00:23:21 +0530

    week-7.1-mongodb

--- week-7-mongoDB/index.js
+++ week-7-mongoDB/index.js
@@ -36,7 +36,7 @@ app.post("/signup", async (req,res)=>{
     // usual /signup endpoints stuffs, user sends the data through the body alongside, and hence we first parses the data 
     // user is expected to send email, username and password using body at the time of signup
     const email = req.body.email
-    const username = req.body.username
+    const name = req.body.username // User schema defines the field as name, not username.
     const password = req.body.password
 
 
@@ -51,7 +51,7 @@ app.post("/signup", async (req,res)=>{
 
     await UserModel.create({
         email: email,
-        username: username,
+        name: name,
         password: password
     })
 
@@ -127,7 +127,9 @@ app.post("/signin", async (req,res)=>{
         */
         
         const token = jwt.sign({
-            id : user._id
+
+            //_id is an object class which has .toString function that we use
+            userId : user._id.toString() // must convert this object id into string to be able to see it 
             // we generated a token using the user's object id 
         }, JWT_SECRET) 
 
@@ -146,13 +148,15 @@ app.post("/signin", async (req,res)=>{
 
 })
 
+
+
 /*
 so far
 
 ~~~~~~~~~~~~ /signup endpoint story ~~~~~~~~~~~
 - we created a /signup endpoint which takes the user credentials and stores it in the database
 -- we have used UserModel which does the transfer of data from this endpoint to users collection in the database. we call .create(..the user data..) on this UserModel - which inserts the user data to the UserModel and it does its work of transferring it at the database.
-(rem that this UserMode.create({..data...}) returns a promise, so we must await this and put it inside a async function)
+(rem that this UserMode.create({..data...}) returns a promise, so we must await this - because without this even if it fails, the code would respond the user that they signedup- and also put it inside a async function)
 --- we then respond back the user that they have signed up.
 
 ~~~~~~~~~~ /signin endpoint story ~~~~~~~~~~~
@@ -177,16 +181,21 @@ NOTE:
 */
 
 
+
+
+
 /*
 
 ~~~~~~~~~ checking if these two endpoints are working or not ~~~~~~~~~~~
 
 - we run this using node index.js 
-- open postman and send a signup request with the email, password and username 
 
-- it should store my credentials inside the database 
+- open postman
 
+- 1.  send a /SIGNUP request with the email, password and username 
+(it should store my credentials inside the database )
 
+output:
 ====> inside todo-naman-2277 collection - we can see the user's credentials stored.
 
 _id: 67b9e8dfc87ba559a8756d24
@@ -196,26 +205,139 @@ __v: 0
 
 
 
+- 2. /SIGNIN endpoint
+
+we put the correct emaid id and password of the user
+## so we get jwt token in the postman as a response
+{
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3YjllOTBkYzg3YmE1NTlhODc1NmQyNiIsImlhdCI6MTc0MDIzNzY5MH0.lepDb4V7Uz2AuwGkrYkk-c6zUP7jKEx_qUlsRjUW2Rw"
+}
+    
+
+## also since we have console.log(user) - we get to see the user complete details stored at the database in the console.
+
+{
+  _id: new ObjectId('67b9e90dc87ba559a8756d26'),
+  email: 'ramesh@gmail.com',
+  password: '112233',
+  __v: 0
+}
+
 NOTE: 
 
 even when sometimes, our backend application crashes- we are not needed to re put the data of user - it persists in the database.
 this is the whole point of database- backend may or may not crash- but data should persist in the database.
+
 */
 
 
 
+// ~~~~~~~~~~ NEXT PARTS OF THE CODE - authenticated endpoints ~~~~~~~~~
 
 
 
 // both the below endpoint will be authenticated - only when user is logged in, can they be able to hit those.
+// at below endpoints - we first do token check (authentication) and basically use TodoModel on them as they are related to todos. 
+
+// to authenticate beforehand the tokens, we will use authMiddleware
+
+// NOTE: we can define middleware anywhere, just keep that in the middle argument of the endpoint where you want to use this
+
+
+
+
+function authMiddleware(req,res,next){
+
+    // we expect the user to give us the token they got in /signin via headers
+    const token = req.headers.token
+
+    // after we fetch the token, we extract the decodedData from the token using verify
+    const decodedInfo = jwt.verify(token, JWT_SECRET) // verify this token the user sent me with my JWT_SECRET - to ensure my backend only gave this user this token or not 
+
+    // if decodedInfo is correct, we can say user is authenticated and then he is allowed to hit the endPoint he wants to hit - we call next()
+    // but also, we want to modify the req and populate it with the userId of the user after it is done checking its authentication.
+
+    // remember - middlewares can modify the req/res objects
+    // that way, it is sending an extra info about the user for the endpoints the user is hitting
+
+
+    if (decodedInfo){
+        // populate the req with this user's id
+        req.userId = decodedInfo.userId
+        // call next()
+        next()
+    } else{
+        res.status(403).json({
+            message: "Incorrect Credentials."
+        })
+    }
+} 
+
+
+
+
 
 // endpoint that user can hit from the frontend to create the new todo 
-app.post("/todo", (req,res)=>{
+app.post("/todo", authMiddleware, async (req,res)=>{
 
+    // at authMiddleware - after authenticating the user- modifies the req and adds the user's id into it and then calls next() - so we can access user id using req.userId here 
+    const userId = req.userId
+
+    // now with this userId - we can get any details about this user - hit the database and find its details
+    // given this userId , how can we put a new todo at this endpoint?
+
+    const title = req.body.title // user sends the title of this new todo - getting it from body
+    const desc = req.body.desc  // user also sends the desc of this new todo - getting it from body
+
+    await TodoModel.create({
+        // we are giving the details of the todo the user wants to create
+        // we trust TodoModel whose sole work is to put data from backend to the database.
+
+        title: title,
+        desc: desc,
+        userId: userId
+
+        // must add userId of the user to the Todo they are creating - it will help us identify this todo belongs to which user.
+
+    })
+
+
+    res.json({
+        message: "new todo created",
+        userId : userId
+    })
+
+
+
+    /* for the moment, just simply returns the userId to the user on hitting this endPoints for understanding purpose.
+     we are not putting a new todo or get the todos in the next endpoint - we simply respond the user with the userId on hitting these for the moment for understanding purpose.
+
+    res.json({
+        userId : userId
+    })
+
+    - it worked ✔✅
+
+
+    */
 })
 
 
-app.get("/todos", (req,res)=>{
+// endpoint to fetch the existing todos
+app.get("/todos", authMiddleware, async (req,res)=>{
+    const userId = req.userId // authmware had put this userId inside req, so we are directly accessing it
+
+    const user = await TodoModel.find({
+        userId: userId
+    })
+
+
+
+    /*
+    // res.json({
+    //     userId : userId
+    // })
+    */
 
 })
 
@@ -229,3 +351,16 @@ app.listen(3000)
 // for that, we will create a new file - db.js and put all the databse logics over there.
 
 // NOTE that we can even put those code here but to structure them better, we want to put them in separate file 
+
+
+/*
+
+---> 1. Await mongoose.connect() before starting the server.
+
+--> 2. at /signup endpoint - make sure we ask the user to give details just as things are set in schema 
+
+- User schema is set in db.js - where it defines email, password and name 
+- so we must ask user to give their details - email, name and password 
+(name and not username - take care of these mistakes)
+
+*/
